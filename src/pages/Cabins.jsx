55c093565dfb17cb44ef12296120a0dd53949493
yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-08-31 17:18:59 +0800

    Added: Updated/create/delete

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,23 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins";
+import Button from "../ui/Button";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
 
 function Cabins() {
-  const [data, setData] = useState([]);
-  useEffect(function () {
-    getCabins().then((data) => setData(data));
-  }, []);
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-      {data.map((data) => (
-        <div className="" key={data.id}>
-          <img src={data.image} alt="" />
-        </div>
-      ))}
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
   );
 }
 
