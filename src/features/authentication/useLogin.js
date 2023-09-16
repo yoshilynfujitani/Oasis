bc2fbb95dd1e3969de88f8bb3c09a1f2a47f9f92
Yoshi.dev <fujitaniyoshilynp@gmail.com>
2023-09-16 10:10:30 +0800

    Added: Protected routes

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useNavigate } from "react-router-dom";
 import { login as loginAPI } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 
 export function useLogin() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginAPI({ email, password }),
 
-    onSuccess: (data) => {
-      console.log(data);
+    onSuccess: (user) => {
+      queryClient.setQueryData(["user"], user.user);
       navigate("/dashboard");
     },
     onError: (err) => {
---
+++ src/ui/ProtectedRouted.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect } from "react";
+import { useUser } from "../features/authentication/useUser";
+import Spinner from "./Spinner";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+const FullPage = styled.div`
+  height: 100vh;
+  background-color: var() (--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const ProtectedRouted = ({ children }) => {
+  const navigate = useNavigate();
+  const { isAuthenticated, isLoading } = useUser();
+
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  if (isAuthenticated) return children;
+};
+
+export default ProtectedRouted;
